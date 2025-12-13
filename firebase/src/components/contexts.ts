973a4dc013e4221e29c54bdcff40b164eb937297
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2025-12-13 12:36:34 -0500

    I have a good feeling about this - new frontend

--- firebase/src/components/contexts.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Model } from "../state/model.ts";
-
-export const FSContext = React.createContext<FS | undefined>(undefined);
-
-export const ModelContext = React.createContext<Model | null>(null);
-
